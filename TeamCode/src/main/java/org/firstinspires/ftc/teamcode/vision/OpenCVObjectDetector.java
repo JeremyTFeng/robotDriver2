a8a5c458607c03868a588965d3f48da3899977ad
wangx <wxiaodong@gmail.com>
2022-09-21 11:01:14 -0700

    capture file in opecv pipeline

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/OpenCVObjectDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/OpenCVObjectDetector.java
@@ -1,11 +1,14 @@
 package org.firstinspires.ftc.teamcode.vision;
 
+import org.firstinspires.ftc.teamcode.util.RobotLogger;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
@@ -18,7 +21,9 @@ public class OpenCVObjectDetector extends OpenCvPipeline {
         RIGHT,
         NONE
     }
-
+    private String TAG = "OpenCVObjectDetector";
+    private String saveImgFileName = "/sdcard/FIRST/capture.jpg";
+    private boolean doneImageCapture = false;
     private int width; // width of the image
     public SkystoneLocation location;
 
@@ -39,6 +44,26 @@ public class OpenCVObjectDetector extends OpenCvPipeline {
         // The detector detects regular stones. The camera fits two stones.
         // If it finds one regular stone then the other must be the skystone.
         // If both are regular stones, it returns NONE to tell the robot to keep looking
+        RobotLogger.dd(TAG, input.rows() + " " + input.cols() + " imaged saved to " + saveImgFileName);
+        if (!doneImageCapture) {
+            Imgcodecs imageCodecs = new Imgcodecs();
+            imageCodecs.imwrite(saveImgFileName, input);
+            doneImageCapture = true;
+        }
+
+        /*  test!!!
+         * Draw a simple box around the middle 1/2 of the entire frame
+
+        Imgproc.rectangle(
+                input,
+                new Point(
+                        input.cols()/4,
+                        input.rows()/4),
+                new Point(
+                        input.cols()*(3f/4f),
+                        input.rows()*(3f/4f)),
+                new Scalar(0, 255, 0), 4);
+        */
 
         // Make a working copy of the input matrix in HSV
         Mat mat = new Mat();
